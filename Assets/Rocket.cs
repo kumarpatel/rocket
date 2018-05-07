7ada6b802e2e92dc436978712b99a0578624300d
Kumar Patel <kumarpatel@Kumars-MacBook-Pro.local>
2018-05-07 16:48:25 -0300

    more rocket setup

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+    Rigidbody rigidbody;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.add
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+        }
+    }
 }
